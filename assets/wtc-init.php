21a0272013dc6853d2756a15e57ed62efde05a00
Ahmad Awais <mrahmadawais@gmail.com>
2016-03-21 18:20:07 +0500

    Titan Framework embedded 💯

--- assets/wtc-init.php
+++ assets/wtc-init.php
@@ -19,3 +19,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 if ( file_exists( WTC_DIR . '/assets/shortcode/shortcode-init.php' ) ) {
     require_once( WTC_DIR . '/assets/shortcode/shortcode-init.php' );
 }
+
+
+/**
+ * Titan Framework Embedder.
+ *
+ * @since 1.0.0
+ */
+if ( file_exists( WTC_DIR . '/assets/titan-framework/framework/titan-framework-embedder.php' ) ) {
+    require_once( WTC_DIR . '/assets/titan-framework/framework/titan-framework-embedder.php' );
+}
